c7a3228c8bcac5779063ef852be454620a77ddc6
Basicula <maksim.basum@gmail.com>
2023-03-12 18:11:54 +0200

    Add different shapes for elements and separate field data

--- src/CanvasUtils.js
+++ src/CanvasUtils.js
@@ -15,3 +15,101 @@ export function draw_rect(context, x, y, width, height, line_width = 1, color = 
   context.stroke();
   context.closePath();
 }
+
+export function draw_regular_polygon(
+  context,
+  center,
+  size,
+  angle_count,
+  start_angle = 0,
+  rounding_radius = 0) {
+
+  if (angle_count === 0) {
+    context.arc(center[0], center[1], size, 0, 2 * Math.PI);
+    return;
+  }
+
+  const angle_step = (Math.PI * 2) / angle_count;
+  var points = [];
+  for (let point_id = 0; point_id < angle_count; ++point_id) {
+    const angle = start_angle + point_id * angle_step;
+    points.push([
+      center[0] + size * Math.cos(angle),
+      center[1] + size * Math.sin(angle),
+    ]);
+  }
+
+  if (rounding_radius === 0) {
+    context.moveTo(points[0][0], points[0][1]);
+    for (let point_id = 1; point_id < points.length; ++point_id)
+      context.lineTo(points[point_id][0], points[point_id][1]);
+    context.lineTo(points[0][0], points[0][1]);
+    return;
+  }
+
+  const point_offset = Math.sin(angle_step / 2) * rounding_radius / Math.sin((Math.PI - angle_step) / 2);
+  var points_with_offset = [];
+  var arc_centers = [];
+  for (let point_id = 0; point_id < points.length; ++point_id) {
+    const curr_point = points[point_id];
+    var prev_point_id = point_id - 1;
+    var next_point_id = point_id + 1;
+    if (point_id === 0)
+      prev_point_id = points.length - 1;
+    if (point_id === points.length - 1)
+      next_point_id = 0;
+    const prev_point = points[prev_point_id];
+    const next_point = points[next_point_id];
+    const vector1 = [prev_point[0] - curr_point[0], prev_point[1] - curr_point[1]];
+    const vector2 = [next_point[0] - curr_point[0], next_point[1] - curr_point[1]];
+    const vector1_length = Math.sqrt(vector1[0] * vector1[0] + vector1[1] * vector1[1]);
+    const vector2_length = Math.sqrt(vector2[0] * vector2[0] + vector2[1] * vector2[1]);
+    const vector1_normalized = [vector1[0] / vector1_length, vector1[1] / vector1_length];
+    const vector2_normalized = [vector2[0] / vector2_length, vector2[1] / vector2_length];
+    points_with_offset.push([
+      curr_point[0] + vector1_normalized[0] * point_offset,
+      curr_point[1] + vector1_normalized[1] * point_offset]);
+    points_with_offset.push([
+      curr_point[0] + vector2_normalized[0] * point_offset,
+      curr_point[1] + vector2_normalized[1] * point_offset]);
+
+    const vector_to_arc_center = [
+      vector1_normalized[0] + vector2_normalized[0],
+      vector1_normalized[1] + vector2_normalized[1]
+    ];
+    const vector_to_arc_center_length = Math.sqrt(
+      vector_to_arc_center[0] * vector_to_arc_center[0] +
+      vector_to_arc_center[1] * vector_to_arc_center[1]
+    );
+    const vector_to_arc_center_normalized = [
+      vector_to_arc_center[0] / vector_to_arc_center_length,
+      vector_to_arc_center[1] / vector_to_arc_center_length
+    ];
+    const dist_to_arc_center = Math.sqrt(rounding_radius * rounding_radius + point_offset * point_offset);
+    arc_centers.push([
+      curr_point[0] + vector_to_arc_center_normalized[0] * dist_to_arc_center,
+      curr_point[1] + vector_to_arc_center_normalized[1] * dist_to_arc_center
+    ]);
+  }
+
+  context.moveTo(points_with_offset[0][0], points_with_offset[0][1]);
+  for (let point_id = 1; point_id < points_with_offset.length; point_id += 2) {
+    const prev_point = point_id - 1;
+    var next_point_id = point_id + 1;
+    if (next_point_id === points_with_offset.length)
+      next_point_id = 0;
+    const arc_center_id = Math.trunc(point_id / 2);
+    const arc_center = arc_centers[arc_center_id];
+    const arc_end_angle = Math.atan2(
+      (points_with_offset[point_id][1] - arc_center[1]),
+      (points_with_offset[point_id][0] - arc_center[0])
+    );
+    const arc_start_angle = Math.atan2(
+      (points_with_offset[prev_point][1] - arc_center[1]),
+      (points_with_offset[prev_point][0] - arc_center[0])
+    );
+    context.arc(arc_center[0], arc_center[1], rounding_radius, arc_start_angle, arc_end_angle);
+    context.lineTo(points_with_offset[point_id][0], points_with_offset[point_id][1]);
+    context.lineTo(points_with_offset[next_point_id][0], points_with_offset[next_point_id][1]);
+  }
+}
--- src/GameField.js
+++ src/GameField.js
@@ -1,230 +1,8 @@
 import { useRef, useEffect, useState } from "react";
 
-import { init_array, manhattan_distance } from "./Utils.js";
-import { draw_line, draw_rect, } from "./CanvasUtils.js";
-
-class FieldData {
-  #field;
-  #width;
-  #height;
-
-  #values_interval;
-  #values_probability_mask;
-
-  #get_random_value() {
-    const random = Math.random();
-    var accumulated_probability = 0;
-    for (let i = 0; i < this.#values_interval.length; ++i) {
-      if (random <= accumulated_probability)
-        return this.#values_interval[i - 1];
-      accumulated_probability += this.#values_probability_mask[i];
-    }
-    return this.#values_interval[this.#values_interval.length - 1];
-  }
-
-  #get_cross_groups() {
-    const check_row = (row_id, column_id) => {
-      var r = column_id + 1;
-      var l = column_id - 1;
-      while (true) {
-        if (r < this.#width &&
-          this.#field[row_id][r] === this.#field[row_id][column_id])
-          ++r;
-        else if (l >= 0 &&
-          this.#field[row_id][l] === this.#field[row_id][column_id])
-          --l;
-        else
-          break;
-      }
-      return [r, l];
-    };
-    const check_column = (row_id, column_id) => {
-      var r = row_id + 1;
-      var l = row_id - 1;
-      while (true) {
-        if (r < this.#height &&
-          this.#field[r][column_id] === this.#field[row_id][column_id])
-          ++r;
-        else if (l >= 0 &&
-          this.#field[l][column_id] === this.#field[row_id][column_id])
-          --l;
-        else
-          break;
-      }
-      return [r, l];
-    };
-    var taken = init_array(this.#width, this.#height, false);
-    var groups = [];
-    for (let row_id = 0; row_id < this.#height; ++row_id) {
-      for (let column_id = 0; column_id < this.#width; ++column_id) {
-        const component_value = this.#field[row_id][column_id];
-        if (component_value === -1)
-          continue;
-        if (taken[row_id][column_id])
-          continue;
-        var group = [];
-        var [row_r, row_l] = check_row(row_id, column_id);
-        var [column_r, column_l] = check_column(row_id, column_id);
-        if (row_r - row_l >= 4) {
-          for (let i = row_l + 1; i < row_r; ++i) {
-            group.push([row_id, i]);
-            taken[row_id][i] = true;
-            var [sub_column_r, sub_column_l] = check_column(row_id, i);
-            if (sub_column_r - sub_column_l >= 4)
-              for (let j = sub_column_l + 1; j < sub_column_r; ++j) {
-                if (j === row_id)
-                  continue;
-                group.push([j, i]);
-                taken[j][i] = true;
-              }
-          }
-        } else if (column_r - column_l >= 4) {
-          for (let i = column_l + 1; i < column_r; ++i) {
-            group.push([i, column_id]);
-            taken[i][column_id] = true;
-            var [sub_row_r, sub_row_l] = check_row(i, column_id);
-            if (sub_row_r - sub_row_l >= 4)
-              for (let j = sub_row_l + 1; j < sub_row_r; ++j) {
-                if (j === column_id)
-                  continue;
-                group.push([i, j]);
-                taken[i][j] = true;
-              }
-          }
-        }
-        else
-          continue;
-        groups.push(group);
-      }
-    }
-    return groups;
-  }
-
-  constructor(width, height) {
-    this.#width = width;
-    this.#height = height;
-
-    this.#values_interval = [1, 2, 4, 8];
-    this.#values_probability_mask = [0.4, 0.3, 0.2, 0.2];
-
-    this.#field = init_array(this.#width, this.#height, undefined, () => {
-      return this.#get_random_value();
-    });
-
-    while (true) {
-      const removed_groups_sizes = this.remove_groups();
-      if (removed_groups_sizes.length === 0)
-        break;
-      this.spawn_new_values();
-    }
-  }
-
-  clone() {
-    var new_field_data = new FieldData(0, 0);
-    new_field_data.#field = this.#field;
-    new_field_data.#width = this.#width;
-    new_field_data.#height = this.#height;
-    new_field_data.#values_interval = this.#values_interval;
-    new_field_data.#values_probability_mask = this.#values_probability_mask;
-    return new_field_data;
-  }
-
-  get width() {
-    return this.#width;
-  }
-
-  get height() {
-    return this.#height;
-  }
-
-  at(row, column) {
-    return this.#field[row][column];
-  }
-
-  remove_groups(count = -1) {
-    const groups = this.#get_cross_groups();
-    if (count === -1)
-      count = groups.length;
-    count = Math.min(count, groups.length);
-    if (count === 0)
-      return [];
-    var group_details = [];
-    for (let group_id = 0; group_id < count; ++group_id) {
-      const group = groups[group_id];
-      group_details.push({
-        size: group.length,
-        value: this.#field[group[0][0]][group[0][1]]
-      });
-      for (let element of group)
-        this.#field[element[0]][element[1]] = -1;
-    }
-    return group_details;
-  }
-
-  move_elements() {
-    for (let column_id = 0; column_id < this.#width; ++column_id) {
-      var empty_row_id = -1;
-      for (let row_id = this.#height - 1; row_id >= 0;) {
-        if (this.#field[row_id][column_id] === -1 && empty_row_id === -1) {
-          empty_row_id = row_id;
-        } else if (empty_row_id !== -1 && this.#field[row_id][column_id] !== -1) {
-          [this.#field[row_id][column_id], this.#field[empty_row_id][column_id]] =
-            [this.#field[empty_row_id][column_id], this.#field[row_id][column_id]];
-          row_id = empty_row_id;
-          empty_row_id = -1;
-        }
-        --row_id;
-      }
-    }
-  }
-
-  spawn_new_values() {
-    for (let row_id = 0; row_id < this.#height; ++row_id)
-      for (let column_id = 0; column_id < this.#width; ++column_id)
-        if (this.#field[row_id][column_id] === -1)
-          this.#field[row_id][column_id] = this.#get_random_value();
-  }
-
-  swap_cells(row1, column1, row2, column2) {
-    if (row1 < 0 || row1 >= this.#height || column1 < 0 || column1 >= this.#width ||
-      row2 < 0 || row2 >= this.#height || column2 < 0 || column2 >= this.#width)
-      return;
-    if (this.#field[row1][column1] === -1)
-      return;
-    if (this.#field[row2][column2] === -1)
-      return;
-    [this.#field[row1][column1], this.#field[row2][column2]] =
-      [this.#field[row2][column2], this.#field[row1][column1]];
-  }
-
-  get_all_available_moves() {
-    const neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-    var moves = [];
-    for (let row_id = 0; row_id < this.#height; ++row_id)
-      for (let column_id = 0; column_id < this.#width; ++column_id)
-        for (let neighbor of neighbors) {
-          const neighbor_row_id = row_id + neighbor[0];
-          const neighbor_column_id = column_id + neighbor[1];
-          if (neighbor_row_id < 0 || neighbor_row_id >= this.#height ||
-            neighbor_column_id < 0 || neighbor_column_id >= this.#width)
-            continue;
-          this.swap_cells(row_id, column_id, neighbor_row_id, neighbor_column_id);
-          if (this.#get_cross_groups().length > 0) {
-            let move_exists = false;
-            for (let move of moves)
-              if (row_id === move[1][0] && column_id === move[1][1] &&
-                neighbor_row_id === move[0][0] && column_id === move[0][1]) {
-                move_exists = true;
-                break;
-              }
-            if (!move_exists)
-              moves.push([[row_id, column_id], [neighbor_row_id, neighbor_column_id]]);
-          }
-          this.swap_cells(row_id, column_id, neighbor_row_id, neighbor_column_id);
-        }
-    return moves;
-  }
-}
+import { FieldData } from "./GameFieldData.js";
+import { manhattan_distance } from "./Utils.js";
+import { draw_line, draw_rect, draw_regular_polygon } from "./CanvasUtils.js";
 
 function map_coordinates(x, y, grid_step) {
   return [
@@ -248,34 +26,59 @@ function render_grid(context, field_data, grid_step) {
   }
 }
 
-function render_element(context, x, y, value, element_size, element_offset, is_highlighted = false) {
+function element_style_by_value(value) {
   var pow = 0;
-  var val = value;
-  while (val > 1) {
+  while (value > 1) {
     ++pow;
-    val = val >> 1;
+    value = value >> 1;
   }
   var color = "#000000";
+  var shape_drawer = (context, x, y, size) => {};
+  const rounding_radius = 7;
   switch (pow) {
     case 0:
       color = "#3DFF53";
+      shape_drawer = (context, x, y, size) => {
+        draw_regular_polygon(context, [x + size / 2, y + 5 * size / 8], 11 * size / 16,
+          3, -Math.PI / 2, rounding_radius);
+      }
       break;
     case 1:
       color = "#FF4828";
+      shape_drawer = (context, x, y, size) => {
+        draw_regular_polygon(context, [x + size / 2, y + size / 2], 10 * size / 14,
+          4, -Math.PI / 4, rounding_radius);
+      }
       break;
     case 2:
       color = "#0008FF";
+      shape_drawer = (context, x, y, size) => {
+        draw_regular_polygon(context, [x + size / 2, y + size / 2], 9 * size / 16,
+          5, -Math.PI / 2, rounding_radius);
+      }
       break;
     case 3:
       color = "#14FFF3";
+      shape_drawer = (context, x, y, size) => {
+        draw_regular_polygon(context, [x + size / 2, y + size / 2], 9 * size / 16,
+          6, 0, rounding_radius);
+      }
       break;
     case 4:
       color = "#FF05FA";
+      shape_drawer = (context, x, y, size) => { }
       break;
     case 5:
       color = "#FFFB28";
+      shape_drawer = (context, x, y, size) => { }
       break;
   }
+  return [color, shape_drawer];
+}
+
+function render_element(context, x, y, value, element_size, element_offset, is_highlighted = false) {
+  const [color, shape_drawer] = element_style_by_value(value);
+
   context.beginPath();
 
   if (!is_highlighted) {
@@ -283,13 +86,15 @@ function render_element(context, x, y, value, element_size, element_offset, is_h
     context.shadowColor = "rgba(0,0,0,1)";
   }
 
-  context.lineWidth = 4;
+  context.lineWidth = 1;
   context.strokeStyle = "#000000";
-  context.rect(x, y, element_size, element_size);
+  shape_drawer(context, x, y, element_size);
+  context.stroke();
+  context.stroke();
   context.stroke();
 
   context.fillStyle = color;
-  context.rect(x, y, element_size, element_size);
+  shape_drawer(context, x, y, element_size);
   context.fill();
 
   context.shadowBlur = 0;
@@ -370,7 +175,7 @@ export default function GameField({ width, height, onStrike }) {
     if (second_element.length > 0)
       highlighted_elements.push(second_element);
     render(context, field_data, grid_step, element_offset, highlighted_elements);
-    setTimeout(update_game_state, 500);
+    setTimeout(update_game_state, 100);
   });
 
   const update_game_state = () => {
