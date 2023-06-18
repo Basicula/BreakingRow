0550ae42c57139f28111b70354aecb9c6eaa89e6
Basicula <maksim.basum@gmail.com>
2023-06-18 17:20:23 +0300

    Add ability to explicitly set svg path closure

--- Assets/Scripts/SVGEntities/SVGPath.cs
+++ Assets/Scripts/SVGEntities/SVGPath.cs
@@ -7,10 +7,12 @@ public class SVGPath : SVGEntity
   public string fill_color;
   public string stroke_color;
   private List<string> m_commands;
+  private bool m_is_closed;
 
   public SVGPath()
   {
     m_commands = new List<string>();
+    m_is_closed = false;
   }
 
   public void MoveTo(Vector2 point)
@@ -29,9 +31,15 @@ public class SVGPath : SVGEntity
     m_commands.Add($"A {rounding_radius} {rounding_radius} 0 0 {arc_direction_value} {point[0]} {point[1]}");
   }
 
+  public void Close()
+  {
+    m_is_closed = true;
+  }
+
   public override string GetXML()
   {
     string path = string.Join(" ", m_commands);
-    return $"<path d=\"{path} z\" fill=\"{fill_color}\" stroke-width=\"{stroke_width}\" stroke=\"{stroke_color}\"/>";
+    var closure = m_is_closed ? " z" : "";
+    return $"<path d=\"{path}{closure}\" fill=\"{fill_color}\" stroke-width=\"{stroke_width}\" stroke=\"{stroke_color}\"/>";
   }
 }
