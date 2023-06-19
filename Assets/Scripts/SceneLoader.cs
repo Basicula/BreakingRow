b26bd2e904c270763681f82025a9beea2fd4824a
Basicula <maksim.basum@gmail.com>
2023-06-19 21:00:41 +0300

    Add main menu scene

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+  public static void LoadScene(string i_name)
+  {
+    SceneManager.LoadScene(i_name);
+  }
+}
---
+++ Assets/Scripts/StartGameMode.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class StartGameMode : MonoBehaviour
+{
+  [SerializeReference] private GameModeSelection m_game_mode_selection;
+
+  public void LoadScene()
+  {
+    SceneLoader.LoadScene($"{m_game_mode_selection.GetCurrentGameModeName()}Game");
+  }
+}
