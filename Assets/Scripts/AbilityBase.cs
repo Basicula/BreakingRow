80cde341c7aed423d14c8085a9509ba2c63f8eab
Max <32364935+Basicula@users.noreply.github.com>
2023-06-24 11:00:18 +0300

    Add cooldown for abilities (#9)
    Make ability prefab more uniform with specific variants for different ability types

--- Assets/Scripts/AbilityBase.cs
+++ Assets/Scripts/AbilityBase.cs
@@ -15,10 +15,14 @@ abstract public class AbilityBase : MonoBehaviour
   [SerializeReference] protected int m_starting_price;
   [SerializeReference] protected int m_price_step;
   [SerializeReference] protected int m_current_price;
+  [SerializeReference] protected int m_cooldown_time;
   [SerializeReference] private GameInfo m_game_info;
 
   protected Button m_button;
   private TMP_Text m_price_text;
+  private Image m_cooldown_overlay;
+  private TMP_Text m_cooldown_timer;
+  private float m_cooldown_start_time;
   private string m_save_file_path;
 
   public void NextPrice()
@@ -35,14 +39,19 @@ abstract public class AbilityBase : MonoBehaviour
         break;
     }
     this._Update();
+    _StartCooldown();
   }
 
   abstract protected void _Init();
 
   private void Start()
   {
+    m_cooldown_start_time = Time.time - m_cooldown_time;
     m_button = gameObject.GetComponent<Button>();
+    m_button.interactable = false;
     m_price_text = transform.GetChild(2).gameObject.GetComponent<TMP_Text>();
+    m_cooldown_overlay = transform.GetChild(3).gameObject.GetComponent<Image>();
+    m_cooldown_timer = transform.GetChild(4).gameObject.GetComponent<TMP_Text>();
     this._Init();
 
     m_save_file_path = Application.persistentDataPath + $"/{SceneManager.GetActiveScene().name}{gameObject.name}Ability.json";
@@ -53,7 +62,18 @@ abstract public class AbilityBase : MonoBehaviour
 
   private void Update()
   {
-    if (m_game_info.score < m_current_price)
+    var elapsed_cooldown_time = Time.time - m_cooldown_start_time;
+    if (elapsed_cooldown_time < m_cooldown_time)
+    {
+      m_cooldown_timer.text = Mathf.CeilToInt(m_cooldown_time - elapsed_cooldown_time).ToString();
+      m_cooldown_overlay.fillAmount = 1.0f - elapsed_cooldown_time / m_cooldown_time;
+    }
+    else if (m_cooldown_timer.IsActive())
+    {
+      m_cooldown_timer.gameObject.SetActive(false);
+      m_cooldown_overlay.gameObject.SetActive(false);
+    }
+    else if (m_game_info.score < m_current_price)
       m_button.interactable = false;
     else
       m_button.interactable = true;
@@ -73,6 +93,16 @@ abstract public class AbilityBase : MonoBehaviour
   {
     m_price_text.text = m_current_price.ToString();
     this._Save();
+  }
+
+  private void _StartCooldown()
+  {
+    m_button.interactable = false;
+    m_cooldown_timer.gameObject.SetActive(true);
+    m_cooldown_overlay.gameObject.SetActive(true);
+    m_cooldown_timer.text = m_cooldown_time.ToString();
+    m_cooldown_overlay.fillAmount = 1.0f;
+    m_cooldown_start_time = Time.time;
   }
 
   struct SerializableData
