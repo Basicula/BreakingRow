dbbd6b06c3c7bc51bfe4c552aed124a584be45ad
Basicula <maksim.basum@gmail.com>
2023-06-28 12:30:54 +0300

    Add field size configuration

---
+++ Assets/Scripts/Configurations.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Configurations : MonoBehaviour
+{
+  [SerializeReference] private GameField m_game_field;
+
+  private IntegerInput m_width_input;
+  private IntegerInput m_height_input;
+  private IntegerInput m_active_elements_count_input;
+
+  void Start()
+  {
+    m_width_input = transform.GetChild(0).gameObject.GetComponent<IntegerInput>();
+    m_height_input = transform.GetChild(1).gameObject.GetComponent<IntegerInput>();
+    m_active_elements_count_input = transform.GetChild(2).gameObject.GetComponent<IntegerInput>();
+
+    m_width_input.value = m_game_field.width;
+    m_width_input.on_value_change = () => _InitGameField();
+
+    m_height_input.value = m_game_field.height;
+    m_height_input.on_value_change = () => _InitGameField();
+
+    m_active_elements_count_input.value = m_game_field.active_elements_count;
+    m_active_elements_count_input.on_value_change = () => _InitGameField();
+  }
+
+  private void _InitGameField()
+  {
+    m_game_field.Init(m_width_input.value, m_height_input.value, m_active_elements_count_input.value);
+  }
+}
---
+++ Assets/Scripts/UI/IntegerInput.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class IntegerInput : MonoBehaviour
+{
+  [SerializeReference] private int m_min;
+  [SerializeReference] private int m_max;
+  [SerializeReference] private int m_value;
+
+  public System.Action on_value_change;
+
+  private TMP_InputField m_input_field;
+
+  private void Start()
+  {
+    var input = gameObject.transform.GetChild(1).gameObject;
+    m_input_field = input.GetComponent<TMP_InputField>();
+    m_input_field.text = m_value.ToString();
+    m_input_field.onValueChanged.AddListener((value_text) => _ChangeValue(int.Parse(value_text)));
+
+    var increment = gameObject.transform.GetChild(2).gameObject;
+    var decrement = gameObject.transform.GetChild(3).gameObject;
+    increment.GetComponent<Button>().onClick.AddListener(() => _ChangeValue(m_value + 1));
+    decrement.GetComponent<Button>().onClick.AddListener(() => _ChangeValue(m_value - 1));
+  }
+
+  private void _ChangeValue(int i_new_value)
+  {
+    if (i_new_value == m_value)
+      return;
+    if (i_new_value < m_min || i_new_value > m_max)
+      return;
+    m_value = i_new_value;
+    m_input_field.text = m_value.ToString();
+    if (!(on_value_change is null))
+      on_value_change();
+  }
+
+  public int value
+  {
+    set
+    {
+      m_value = value;
+      m_input_field.text = m_value.ToString();
+    }
+    get => m_value;
+  }
+}
