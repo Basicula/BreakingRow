23da0fdc849dc491e9f0572b436fa91df3622cbc
Basicula <maksim.basum@gmail.com>
2023-11-04 21:23:37 +0200

    Refactor element style provider and add new field element

--- Assets/Scripts/ElementStyleProvider.cs
+++ Assets/Scripts/ElementStyleProvider.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 
 public class ElementStyleProvider
@@ -15,10 +16,52 @@ public class ElementStyleProvider
     public string[] colors;
     public List<SVGPath> paths;
     public Dictionary<int, ElementProps> sprite_cache;
+
+    public ElementProps Get(int i_value, float i_size, float i_line_width)
+    {
+      int value = i_value;
+      if (sprite_cache.ContainsKey(value))
+        return sprite_cache[value];
+      SVG svg = new SVG();
+      {
+        // Unity ignores this information but leave it as it's needed for correct SVG
+        svg.width = i_size;
+        svg.height = i_size;
+        svg.viewbox_min = new Vector2(-i_size / 2, -i_size / 2);
+        svg.viewbox_size = new Vector2(i_size, i_size);
+      }
+      SVGPath path = paths[value % paths.Count];
+      path.fill_color = colors[value % colors.Length];
+      path.stroke_props = new SVGStrokeProps("#000000", i_line_width);
+      svg.Add(path);
+
+      var element_props = new ElementProps();
+      _FillElementNumberText(ref element_props, value);
+      element_props.sprite = SVG.BuildSprite(svg, i_size / 100, i_size / 100);
+      element_props.text_zone_size = i_size * 0.5f;
+      sprite_cache[value] = element_props;
+      return sprite_cache[value];
+    }
+
+    private void _FillElementNumberText(ref ElementProps io_element_props, int i_value)
+    {
+      if (i_value > 20)
+      {
+        char[] exponents = new char[10] { '⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹' };
+        string exponent = "";
+        while (i_value > 0)
+        {
+          exponent = exponents[i_value % 10] + exponent;
+          i_value /= 10;
+        }
+        io_element_props.number = $"2{exponent}";
+      }
+      else
+        io_element_props.number = Mathf.FloorToInt(Mathf.Pow(2, i_value)).ToString();
+    }
   }
 
-  private ElementStyleLibrary m_common_element_style_library;
-  private ElementStyleLibrary m_interactable_element_style_library;
+  private Dictionary<FieldElement.Type, ElementStyleLibrary> m_element_style_library;
   private float m_size;
   private float m_line_width;
   private ShapeProvider m_shape_provider;
@@ -34,91 +77,30 @@ public class ElementStyleProvider
 
   public ElementProps Get(FieldElement i_element)
   {
-    switch (i_element.type)
-    {
-      case FieldElement.Type.Common:
-        return _GetPropsForCommonElement(i_element);
-      case FieldElement.Type.InteractableDestractable:
-        return _GetPropsForInteractableElement(i_element);
-      case FieldElement.Type.Hole:
-        return _GetPropsForHoleElement(i_element);
-      default:
-        throw new System.NotImplementedException($"Element class id={i_element.type} hasn't implemented style props");
-    }
-  }
-
-  private ElementProps _GetPropsForCommonElement(FieldElement i_element)
-  {
-    int value = i_element.value;
-    if (m_common_element_style_library.sprite_cache.ContainsKey(value))
-      return m_common_element_style_library.sprite_cache[value];
-    SVG svg = new SVG();
-    {
-      // Unity ignores this information but leave it as it's needed for correct SVG
-      svg.width = m_size;
-      svg.height = m_size;
-      svg.viewbox_min = new Vector2(-m_size / 2, -m_size / 2);
-      svg.viewbox_size = new Vector2(m_size, m_size);
-    }
-    SVGPath path = m_common_element_style_library.paths[value % m_common_element_style_library.paths.Count];
-    path.fill_color = m_common_element_style_library.colors[value % m_common_element_style_library.colors.Length];
-    path.stroke_props = new SVGStrokeProps("#000000", m_line_width);
-    svg.Add(path);
-
-    var element_props = new ElementProps();
-    _FillElementNumberText(ref element_props, value);
-    element_props.sprite = SVG.BuildSprite(svg, m_size / 100, m_size / 100);
-    element_props.text_zone_size = m_size * 0.5f;
-    m_common_element_style_library.sprite_cache[value] = element_props;
-    return m_common_element_style_library.sprite_cache[value];
-  }
-
-  private ElementProps _GetPropsForInteractableElement(FieldElement i_element)
-  {
-    int value = i_element.value;
-    if (m_interactable_element_style_library.sprite_cache.ContainsKey(value))
-      return m_interactable_element_style_library.sprite_cache[value];
-    SVG svg = new SVG();
-    {
-      // Unity ignores this information but leave it as it's needed for correct SVG
-      svg.width = m_size;
-      svg.height = m_size;
-      svg.viewbox_min = new Vector2(-m_size / 2, -m_size / 2);
-      svg.viewbox_size = new Vector2(m_size, m_size);
-    }
-    SVGPath path = m_interactable_element_style_library.paths[value % m_interactable_element_style_library.paths.Count];
-    path.fill_color = m_interactable_element_style_library.colors[value % m_interactable_element_style_library.colors.Length];
-    path.stroke_props = new SVGStrokeProps("#000000", m_line_width);
-    svg.Add(path);
-
-    var element_props = new ElementProps();
-    _FillElementNumberText(ref element_props, value);
-    element_props.sprite = SVG.BuildSprite(svg, m_size / 100, m_size / 100);
-    element_props.text_zone_size = m_size * 0.5f;
-    m_interactable_element_style_library.sprite_cache[value] = element_props;
-    return m_interactable_element_style_library.sprite_cache[value];
-  }
-
-  private ElementProps _GetPropsForHoleElement(FieldElement i_element)
-  {
-    return new ElementProps();
+    if (i_element.type == FieldElement.Type.Hole)
+      return new ElementProps();
+    if (!m_element_style_library.ContainsKey(i_element.type))
+      throw new System.NotImplementedException($"Element class id={i_element.type} hasn't implemented style props");
+    return m_element_style_library[i_element.type].Get(i_element.value, m_size, m_line_width);
   }
 
   private void _InitLibraries()
   {
+    m_element_style_library = new Dictionary<FieldElement.Type, ElementStyleLibrary>();
     _InitCommonElementLibrary();
-    _InitInteractableElementLibrary();
+    _InitInteractableDestractableElementLibrary();
+    _InitImmobileDestractableElementLibrary();
   }
 
   private void _InitCommonElementLibrary()
   {
-    m_common_element_style_library = new ElementStyleLibrary();
-    m_common_element_style_library.colors = new string[]
+    var library = new ElementStyleLibrary();
+    library.colors = new string[]
     {
       "#3DFF53", "#FF4828", "#0008FF", "#14FFF3", "#FF05FA",
       "#FFFB28", "#FF6D0A", "#CB0032", "#00990A", "#990054"
     };
-    m_common_element_style_library.sprite_cache = new Dictionary<int, ElementProps>();
+    library.sprite_cache = new Dictionary<int, ElementProps>();
 
     var corner_rounding_radius = Mathf.Floor(0.1f * m_size);
     var edge_rounding_radius = Mathf.Floor(2 * m_size);
@@ -179,7 +161,7 @@ public class ElementStyleProvider
     SVGPath rounded_convex_edges_star_7_path = m_shape_provider.RoundEdgesShape(current_shape, star_edge_rounding_radius, false);
     SVGPath rounded_concave_edges_star_7_path = m_shape_provider.RoundEdgesShape(current_shape, star_edge_rounding_radius, true);
 
-    m_common_element_style_library.paths = new List<SVGPath>() {
+    library.paths = new List<SVGPath>() {
       triangle_path, square_path, pentagon_path, star_5_path,
       rotated_triangle_path, hexagon_path, rotated_square_path,
       octagon_path, star_7_path,
@@ -196,34 +178,32 @@ public class ElementStyleProvider
       rounded_concave_edges_star_5_path, rounded_concave_edges_rotated_triangle_path, rounded_concave_edges_hexagon_path,
       rounded_concave_edges_rotated_square_path, rounded_concave_edges_octagon_path, rounded_concave_edges_star_7_path,
     };
+    m_element_style_library[FieldElement.Type.Common] = library;
   }
 
-  private void _InitInteractableElementLibrary()
+  private void _InitInteractableDestractableElementLibrary()
   {
-    m_interactable_element_style_library = new ElementStyleLibrary();
-    m_interactable_element_style_library.colors = new string[]
+    var library = new ElementStyleLibrary();
+    library.colors = new string[]
     {
       "#111111"
     };
-    m_interactable_element_style_library.sprite_cache = new Dictionary<int, ElementProps>();
-    m_interactable_element_style_library.paths = new List<SVGPath>()
+    library.sprite_cache = new Dictionary<int, ElementProps>();
+    library.paths = new List<SVGPath>()
     { m_shape_provider.RegularShape(ShapeProvider.ShapeType.Square)};
+    m_element_style_library[FieldElement.Type.InteractableDestractable] = library;
   }
 
-  private void _FillElementNumberText(ref ElementProps io_element_props, int i_value)
+  private void _InitImmobileDestractableElementLibrary()
   {
-    if (i_value > 20)
+    var library = new ElementStyleLibrary();
+    library.colors = new string[]
     {
-      char[] exponents = new char[10] { '⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹' };
-      string exponent = "";
-      while (i_value > 0)
-      {
-        exponent = exponents[i_value % 10] + exponent;
-        i_value /= 10;
-      }
-      io_element_props.number = $"2{exponent}";
-    }
-    else
-      io_element_props.number = Mathf.FloorToInt(Mathf.Pow(2, i_value)).ToString();
+      "#aaaaaa"
+    };
+    library.sprite_cache = new Dictionary<int, ElementProps>();
+    library.paths = new List<SVGPath>()
+    { m_shape_provider.RegularShape(ShapeProvider.ShapeType.Circle)};
+    m_element_style_library[FieldElement.Type.ImmobileDestractable] = library;
   }
 }
--- Assets/Scripts/FieldElementsFactory.cs
+++ Assets/Scripts/FieldElementsFactory.cs
@@ -14,8 +14,10 @@ public class FieldElementsFactory
         return new FieldElement(i_type, false, false, false, false, false, i_value);
       case FieldElement.Type.InteractableDestractable:
         return new FieldElement(i_type, true, true, true, false, true, i_value);
+      case FieldElement.Type.ImmobileDestractable:
+        return new FieldElement(i_type, false, false, true, false, true, i_value);
       default:
-        return null;
+        throw new System.NotImplementedException($"Field elements factory can't create element with type {i_type} as it's not implemented yet");
     }
   }
 }
